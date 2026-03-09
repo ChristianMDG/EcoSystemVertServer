4ebd925eda42708eed12e903e3dd2e9e408b3584
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-03-09 17:34:37 +0300

    feat(auth) : finish with rolemiddleware to protect route

--- backend/src/middlewares/role.middleware.ts
+++ backend/src/middlewares/role.middleware.ts
@@ -1,15 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
-export const authorizeRole = (role: "ADMIN" | "CLIENT") => {
-    return (req: Request, res: Response, next: NextFunction) => {
-  
-      if (!req.user) {
-        return res.status(401).json({ message: "Unauthorized" });
-      }
-  
-      if (req.user.role !== role) {
-        return res.status(403).json({ message: "Forbidden" });
-      }
-  
-      next();
-    };
+
+const forbidden = (res: Response, message: string) => {
+  res.status(403).json({ message });
+};
+
+export const authorize = (roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user) return forbidden(res, 'No user info found');
+
+    if (!roles.includes(req.user.role)) {
+      return forbidden(res, 'Access denied: insufficient permissions');
+    }
+
+    next();
   };
+};
